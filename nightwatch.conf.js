0dd9d9888ba934ee04d667b3d0b3ba412abdec1b
Anselmo <anselmo.margelo@outlook.com>
2020-07-27 17:18:46 -0300

    alterando para um teste executado

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,77 +1,75 @@
-require('babel-core/register')
+require('babel-core/register');
 
 const chromedriver = require('chromedriver');
-require('geckodriver')
+require('geckodriver');
 
-const testUrl =  'http://zombie-web:5000'
-const defaultTimeout = 15000
+const testUrl = 'http://zombie-web:5000';
+const defaultTimeout = 15000;
 
 module.exports = {
-    src_folders: ['tests'],
+  src_folders: ['tests'],
 
-    page_objects_path: './pages',
-    globals_path: './hooks/globals.js',
+  page_objects_path: './pages',
+  globals_path: './hooks/globals.js',
 
-    webdriver: {
-        start_process: true,
+  webdriver: {
+    start_process: true,
+  },
+  test_workers: {
+    enabled: true,
+    workers: 1,
+  },
+
+  test_settings: {
+    default: {
+      launch_url: testUrl,
+      globals: {
+        waitForConditionTimeout: defaultTimeout, // As vezes a conexão com a internet/rede fica lenta
+      },
+      webdriver: {
+        server_path: chromedriver.path,
+        port: 9515,
+      },
+      desiredCapabilities: {
+        browserName: 'chrome',
+      },
     },
-    test_workers: {
-        enabled: true,
-        workers: 2
+
+    headless: {
+      launch_url: testUrl,
+      globals: {
+        waitForConditionTimeout: defaultTimeout, // As vezes a conexão com a internet/rede fica lenta
+      },
+      webdriver: {
+        server_path: chromedriver.path,
+        port: 9515,
+      },
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          w3c: false,
+          args: ['--headless', '--no-sandbox'],
+        },
+      },
     },
 
-    test_settings: {
-        default: {
-            launch_url: testUrl,
-            globals: {
-                waitForConditionTimeout: defaultTimeout // As vezes a conexão com a internet/rede fica lenta
-            },
-            webdriver: {
-                server_path: chromedriver.path,
-                port: 9515
-            },
-            desiredCapabilities: {
-                browserName: "chrome"
-            }
-        },
+    firefox: {
+      launch_url: testUrl,
+      globals: {
+        waitForConditionTimeout: defaultTimeout, // As vezes a conexão com a internet/rede fica lenta
+      },
+      webdriver: {
+        server_path: '.\\node_modules\\.bin\\geckodriver.cmd',
+        port: 4444,
+      },
+      desiredCapabilities: {
+        browserName: 'firefox',
+        acceptInsecureCerts: true,
+      },
+    },
 
-        headless: {
-            launch_url: testUrl,
-            globals: {
-                waitForConditionTimeout: defaultTimeout // As vezes a conexão com a internet/rede fica lenta
-            },
-            webdriver: {
-                server_path: chromedriver.path,
-                port: 9515
-            },
-            desiredCapabilities: {
-                browserName: "chrome",
-                chromeOptions: {
-                    w3c: false,
-                    args: ['--headless', '--no-sandbox']
-                }
-            }
-        },
-
-        firefox: {
-            launch_url: testUrl,
-            globals: {
-                waitForConditionTimeout: defaultTimeout // As vezes a conexão com a internet/rede fica lenta
-            },
-            webdriver: {
-                server_path: '.\\node_modules\\.bin\\geckodriver.cmd',
-                port: 4444
-            },
-            desiredCapabilities: {
-                browserName: "firefox",
-                acceptInsecureCerts: true
-            }
-        },
-
-        stage: {
-            launch_url: "http://stage.zombieplus.com.br"
-        }
-    }
-
-
-}
+    stage: {
+      launch_url: 'http://stage.zombieplus.com.br',
+    },
+  },
+};
--- tests/Busca/404.test.js
+++ tests/Busca/404.test.js
@@ -1,34 +1,30 @@
-
-
 module.exports = {
-    '@tags':['smoke', '404'],
+  '@tags': ['smoke', '404'],
 
-    before: function (browser) {
-        let login = browser.page.login()
-        let sidebar = browser.page.sidebar()
+  before: function (browser) {
+    let login = browser.page.login();
+    let sidebar = browser.page.sidebar();
 
-        login.with('zumbi@dospalmares.com.br', 'pwd123')
-        sidebar.expectLoggedUser('Quilombo')
+    login.with('zumbi@dospalmares.com.br', 'pwd123');
+    sidebar.expectLoggedUser('Quilombo');
+  },
 
-    },
+  'quando eu busco um titulo não cadastrado': function (browser) {
+    let movie = browser.page.movie();
 
-    'quando eu busco um titulo não cadastrado': function (browser) {
-        let movie = browser.page.movie()
+    movie
+      .setValue('@searchInput', 'Não e mais um besteirol Americano')
+      .click('@searchIcon');
+  },
 
-        movie
-            .setValue('@searchInput', 'Não e mais um besteirol Americano')
-            .click('@searchIcon')
+  'entao devo ver uma mensagem de alerta': function (browser) {
+    let movie = browser.page.movie();
 
-    },
-
-    'entao devo ver uam mensagem de alerta': function (browser) {
-        let movie = browser.page.movie()
-
-        movie
-            .waitForElementVisible('@alertDanger', 15000)
-            .assert.containsText('@alertDanger', 'Puxa! não encontramos nada aqui :(')
-
-    }
-
-
-}
+    movie
+      .waitForElementVisible('@alertDanger', 15000)
+      .assert.containsText(
+        '@alertDanger',
+        'Puxa! não encontramos nada aqui :('
+      );
+  },
+};
